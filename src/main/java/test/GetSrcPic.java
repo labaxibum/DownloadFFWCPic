6b172cb85036177190a3d55bb26ea8cf3db1263c
labaxibum <labaxibum216@gmail.com>
2023-06-26 19:57:47 +0700

    adding test sonarqube

--- src/main/java/test/GetSrcPic.java
+++ src/main/java/test/GetSrcPic.java
@@ -1,23 +1,15 @@
 package test;
 
 import base.BaseClass;
-import extensions.FileExtension;
 import org.testng.annotations.Test;
 import pageobject.HomePage;
-import pageobject.SquadPage;
-import pageobject.TeamDetailPage;
 import pageobject.TeamPage;
 
 import java.io.IOException;
 
 public class GetSrcPic extends BaseClass{
-
-    FileExtension fileExtension;
-
     @Test
     public void getSrcOfPic() throws IOException, InterruptedException {
-
-        //driver.get("https://www.pixiv.net/en/artworks/337");
         TeamPage teamPage = new TeamPage(driver);
         HomePage homePage = new HomePage(driver);
         homePage.clickTeamHeader();
